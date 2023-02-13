72884fdccfee6e9de3780e1fb486763c017bed23
alex1431999 <alex@baserow.io>
2023-02-13 12:38:59 +0100

    Make page selectable

--- web-frontend/modules/builder/store/page.js
+++ web-frontend/modules/builder/store/page.js
@@ -1,3 +1,6 @@
+import { StoreItemLookupError } from '@baserow/modules/core/errors'
+import { BuilderApplicationType } from '@baserow/modules/builder/applicationTypes'
+
 export function populatePage(page) {
   page._ = {
     selected: false,
@@ -5,11 +8,48 @@ export function populatePage(page) {
   return page
 }
 
-const state = {}
+const state = {
+  selected: {},
+}
 
-const mutations = {}
+const mutations = {
+  SET_SELECTED(state, { builder, page }) {
+    Object.values(builder.pages).forEach((item) => {
+      item._.selected = false
+    })
+    page._.selected = true
+    state.selected = page
+  },
+}
 
-const actions = {}
+const actions = {
+  async selectById({ commit, dispatch }, { builderId, pageId }) {
+    const builder = await dispatch('application/selectById', builderId, {
+      root: true,
+    })
+    const type = BuilderApplicationType.getType()
+
+    // Check if the just selected application is a builder
+    if (builder.type !== type) {
+      throw new StoreItemLookupError(
+        `The application doesn't have the required ${type} type.`
+      )
+    }
+
+    // Check if the provided page id is found in the just selected builder.
+    const index = builder.pages.findIndex((item) => item.id === pageId)
+    if (index === -1) {
+      throw new StoreItemLookupError(
+        'The page is not found in the selected application.'
+      )
+    }
+    const page = builder.pages[index]
+
+    commit('SET_SELECTED', { builder, page })
+
+    return { builder, page }
+  },
+}
 
 const getters = {}
 

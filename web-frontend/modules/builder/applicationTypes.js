35f2333af0705bba2dc4d3657d23ceaa5daaefca
alex1431999 <alex@baserow.io>
2023-02-14 13:44:19 +0100

    Auto select page when builder is created

--- web-frontend/modules/builder/applicationTypes.js
+++ web-frontend/modules/builder/applicationTypes.js
@@ -32,7 +32,33 @@ export class BuilderApplicationType extends ApplicationType {
 
   populate(application) {
     const values = super.populate(application)
-    values.pages.forEach((object, tables) => populatePage(object))
+    values.pages.forEach((object, pages) => populatePage(object))
     return values
+  }
+
+  select(application, { $router, $i18n, $store }, callback = null) {
+    const pages = application.pages
+      .map((p) => p)
+      .sort((a, b) => a.order - b.order)
+
+    if (pages.length > 0) {
+      $router.push(
+        {
+          name: 'builder-page',
+          params: {
+            builderId: application.id,
+            pageId: pages[0].id,
+          },
+        },
+        callback
+      )
+      return true
+    } else {
+      $store.dispatch('notification/error', {
+        title: $i18n.t('applicationType.cantSelectPageTitle'),
+        message: $i18n.t('applicationType.cantSelectPageDescription'),
+      })
+      return false
+    }
   }
 }

2a1d0fb0193e5297623a5a4a8efd42c60bbd3a2b
alex1431999 <alex@baserow.io>
2023-02-16 12:37:27 +0100

    Add listeners to ws file

--- backend/src/baserow/contrib/builder/ws/signals.py
+++ backend/src/baserow/contrib/builder/ws/signals.py
@@ -1,3 +1,3 @@
-from .page.signals import page_created
+from .page.signals import page_created, page_deleted, page_updated, page_reordered
 
-__all__ = ["page_created"]
+__all__ = ["page_created", "page_deleted", "page_updated", "page_reordered"]

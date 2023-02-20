657169b9293b55541b2d6801aeeaa4d4013692d4
alex1431999 <alex@baserow.io>
2023-02-20 12:32:26 +0100

    Add docstrings for handlers

--- backend/src/baserow/contrib/builder/page/handler.py
+++ backend/src/baserow/contrib/builder/page/handler.py
@@ -6,22 +6,51 @@ from baserow.contrib.builder.page.models import Page
 
 
 class PageHandler:
-    def get_page(self, page_id: int):
+    def get_page(self, page_id: int) -> Page:
+        """
+        Gets a page by ID
+
+        :param page_id: The ID of the page
+        :return: The model instance of the Page
+        """
+
         try:
             return Page.objects.get(id=page_id)
         except Page.DoesNotExist:
             raise PageDoesNotExist()
 
     def create_page(self, builder: Builder, name: str) -> Page:
+        """
+        Creates a new page
+
+        :param builder: The builder the page belongs to
+        :param name: The name of the page
+        :return: The newly created page instance
+        """
+
         last_order = Page.get_last_order(builder)
         page = Page.objects.create(builder=builder, name=name, order=last_order)
 
         return page
 
     def delete_page(self, page: Page):
+        """
+        Deletes the page provided
+
+        :param page: The page that's supposed to be deleted
+        """
+
         page.delete()
 
-    def update_page(self, page: Page, values: Dict):
+    def update_page(self, page: Page, values: Dict) -> Page:
+        """
+        Updates fields of a page
+
+        :param page: The page that should be updated
+        :param values: The fields that should be updated with their corresponding value
+        :return: The updated page
+        """
+
         for key, value in values.items():
             setattr(page, key, value)
 
@@ -29,7 +58,19 @@ class PageHandler:
 
         return page
 
-    def order_pages(self, builder: Builder, order: List[int], base_qs=None):
+    def order_pages(
+        self, builder: Builder, order: List[int], base_qs=None
+    ) -> List[int]:
+        """
+        Assigns a new order to the pages in a builder application.
+        You can provide a base_qs for pre-filter the pages affected by this change.
+
+        :param builder: The builder that the pages belong to
+        :param order: The new order of the pages
+        :param base_qs: A QS that can have filters already applied
+        :return: The new order of the pages
+        """
+
         if base_qs is None:
             base_qs = Page.objects.filter(builder=builder)
 

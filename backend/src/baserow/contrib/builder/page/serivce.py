3e5c969ab4a882064d4cd5427432b7711e83660c
alex1431999 <alex@baserow.io>
2023-02-20 12:37:01 +0100

    Add doc strings to service

--- backend/src/baserow/contrib/builder/page/serivce.py
+++ backend/src/baserow/contrib/builder/page/serivce.py
@@ -26,7 +26,15 @@ class PageService:
     def __init__(self):
         self.handler = PageHandler()
 
-    def get_page(self, user: AbstractUser, page_id: int):
+    def get_page(self, user: AbstractUser, page_id: int) -> Page:
+        """
+        Gets a page by ID
+
+        :param user: The user requesting the page
+        :param page_id: The ID of the page
+        :return: The model instance of the Page
+        """
+
         page = self.handler.get_page(page_id)
 
         CoreHandler().check_permissions(
@@ -39,6 +47,15 @@ class PageService:
         return page
 
     def create_page(self, user: AbstractUser, builder: Builder, name: str) -> Page:
+        """
+        Creates a new page
+
+        :param user: The user trying to create the page
+        :param builder: The builder the page belongs to
+        :param name: The name of the page
+        :return: The newly created page instance
+        """
+
         CoreHandler().check_permissions(
             user,
             CreatePageOperationType.type,
@@ -53,6 +70,13 @@ class PageService:
         return page
 
     def delete_page(self, user: AbstractUser, page: Page):
+        """
+        Deletes the page provided
+
+        :param user: The user trying to delte the page
+        :param page: The page that's supposed to be deleted
+        """
+
         CoreHandler().check_permissions(
             user,
             DeletePageOperationType.type,
@@ -64,7 +88,16 @@ class PageService:
 
         page_deleted.send(self, builder=page.builder, page_id=page.id, user=user)
 
-    def update_page(self, user: AbstractUser, page: Page, values: Dict):
+    def update_page(self, user: AbstractUser, page: Page, values: Dict) -> Page:
+        """
+        Updates fields of a page
+
+        :param user: The user trying to update the page
+        :param page: The page that should be updated
+        :param values: The fields that should be updated with their corresponding value
+        :return: The updated page
+        """
+
         CoreHandler().check_permissions(
             user,
             UpdatePageOperationType.type,
@@ -81,6 +114,15 @@ class PageService:
         return page
 
     def order_pages(self, user: AbstractUser, builder: Builder, order: List[int]):
+        """
+        Assigns a new order to the pages in a builder application.
+
+        :param user: The user trying to order the pages
+        :param builder: The builder that the pages belong to
+        :param order: The new order of the pages
+        :return: The new order of the pages
+        """
+
         CoreHandler().check_permissions(
             user,
             OrderPagesBuilderOperationType.type,

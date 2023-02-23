977d6db1ef703efb79ac42752b151b15fe33a99d
alex1431999 <alex@baserow.io>
2023-02-23 09:57:29 +0100

    Add docstring to order

--- backend/src/baserow/contrib/builder/elements/handler.py
+++ backend/src/baserow/contrib/builder/elements/handler.py
@@ -12,6 +12,14 @@ from baserow.core.utils import extract_allowed
 
 class ElementHandler:
     def get_element(self, element_id: int) -> Element:
+        """
+        Returns an element instance from the database
+
+        :param element_id: The ID of the element
+        :raises ElementDoesNotExist: If the element can't be found
+        :return: The element instance
+        """
+
         try:
             element = Element.objects.select_related(
                 "page", "page__builder", "page__builder__group"
@@ -22,11 +30,27 @@ class ElementHandler:
         return element
 
     def get_elements(self, page: Page) -> List[Element]:
+        """
+        Gets all the elements of a given page.
+
+        :param page: The page that holds the elements
+        :return: The elements of that page
+        """
+
         return Element.objects.filter(page=page)
 
     def create_element(
         self, element_type: ElementType, page: Page, **kwargs
     ) -> Element:
+        """
+        Creates a new element for a page
+
+        :param element_type: The type of the element
+        :param page: The page the element exists in
+        :param kwargs: Additional attributes of the element
+        :return: The created element
+        """
+
         model_class = element_type.model_class
 
         last_order = model_class.get_last_order(page)
@@ -36,9 +60,22 @@ class ElementHandler:
         return element
 
     def delete_element(self, element: Element):
+        """
+        Deletes an element
+        :param element: The to-be-deleted element
+        """
+
         element.delete()
 
     def update_element(self, element: Element, values: Dict[str, Any]) -> Element:
+        """
+        Updates and element with values. Will also check if the values are allowed
+        to be set on the element first.
+
+        :param element: The element that should be updated
+        :param values: The values that should be set on the element
+        :return: The updated element
+        """
 
         shared_allowed_fields = ["config"]
 
@@ -56,6 +93,14 @@ class ElementHandler:
         return element
 
     def order_elements(self, page: Page, new_order: List[int]) -> List[int]:
+        """
+        Orders the elements of a page in a new order
+
+        :param page: The page the elements exist on
+        :param new_order: The new order which they should have
+        :return: The full order of all elements after they have been ordered
+        """
+
         all_elements = Element.objects.filter(page=page)
 
         full_order = Element.order_objects(all_elements, new_order)
